b8dfdf50c84aa53907d0f17c06074ac350c83286
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2021-08-19 14:24:12 -0500

    add address copy component on profile page

--- components/App/CreatorSidebar.js
+++ components/App/CreatorSidebar.js
@@ -1,6 +1,9 @@
+import CopyAddress from './CopyAddress'
+
 export default function CreatorHeader({ creator }) {
   return (
-    <div className="px-6 py-12 w-80">
+    <div className="px-6 py-2 w-80">
+      <CopyAddress address={creator.address} />
       <span className="text-2xl font-semibold">
         {creator.name || 'Anonymous'}
       </span>
--- pages/creators/[address].js
+++ pages/creators/[address].js
@@ -17,7 +17,7 @@ export default function CreatorPage({ creator, items }) {
       <CreatorHeader creator={creator} />
       <div className="pt-12 lg:pt-80 flex lg:flex-nowrap flex-wrap">
         <CreatorSidebar creator={creator} />
-        <div className="px-4 min-h-full w-full overflow-hidden grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+        <div className="px-4 min-h-full w-full overflow-hidden grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:gap-x-8">
           {items.map((item) => (
             <ItemCard key={`${item.hash}-${item.id}`} item={item} />
           ))}
