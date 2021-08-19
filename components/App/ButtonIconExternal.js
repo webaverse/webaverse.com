9dce2f50044235693c583fd34ca33d5da48fd552
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2021-08-19 13:33:34 -0500

    fix external button styling

--- components/App/ButtonIconExternal.js
+++ components/App/ButtonIconExternal.js
@@ -4,13 +4,18 @@ export default function ButtonIconExternal({ icon, text, url }) {
   return (
     <button
       type="button"
-      className="w-64 text-black inline-flex items-center px-6 py-3 border border-transparent shadow-sm text-base font-medium bg-white overflow-hidden rounded-md transform transition ease-in duration-100 hover:-translate-y-0.5 hover:shadow-lg"
+      className="w-64 text-black inline-flex items-center px-6 py-3 my-1 border border-transparent shadow-sm text-base font-medium bg-white overflow-hidden rounded-md transform transition ease-in duration-100 hover:-translate-y-0.5 hover:shadow-lg"
     >
-      <a href={url} target="_blank" className="py-2" rel="noreferrer">
+      <a
+        href={url}
+        target="_blank"
+        className="py-2 w-full flex"
+        rel="noreferrer"
+      >
         {icon}
         <span className="max-w-md">{text}</span>
         <ExternalLinkIcon
-          className="text-gray-300 self-end l-0 h-5 w-5"
+          className="text-gray-300 ml-auto self-end l-0 h-5 w-5"
           aria-hidden="true"
         />
       </a>
