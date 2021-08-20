174be914cde0be0f906834c195e9a915b220f24e
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2021-08-20 15:53:15 -0500

    add ga tag to page

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import NProgress from 'nprogress'
 import Navbar from '../components/Navbar/Navbar'
 import Footer from '../components/Shared/Footer'
 import '../style/nprogress.css'
+import Analytics from '../components/Shared/Analytics'
 
 Router.events.on('routeChangeStart', () => NProgress.start())
 Router.events.on('routeChangeComplete', () => NProgress.done())
@@ -12,6 +13,7 @@ Router.events.on('routeChangeError', () => NProgress.done())
 function MyApp({ Component, pageProps }) {
   return (
     <div className="flex">
+      <Analytics />
       <div className="flex-grow bg-gray-100">
         <Navbar />
         <div className="max-w-screen-2xl mx-auto min-h-screen pt-20">
