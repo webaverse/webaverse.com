6237ce7853d07e9d9139872862013ec5d05bc765
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2021-08-20 12:19:29 -0500

    add view on etherscan button

--- pages/assets/[...asset].js
+++ pages/assets/[...asset].js
@@ -47,6 +47,21 @@ export default function Item({ item, creator }) {
               ))}
           </div>
           <div className="py-4 flex flex-col">
+            <ButtonIconExternal
+              icon={
+                <svg
+                  className="-ml-1 mr-3 h-5 w-5"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M4.364 9.999a.89.89 0 01.895-.89l1.482.004a.891.891 0 01.891.892v5.607c.167-.05.381-.102.616-.157a.743.743 0 00.572-.723V7.776a.892.892 0 01.892-.892h1.485a.891.891 0 01.891.892v6.456s.372-.15.734-.304a.744.744 0 00.454-.685V5.547a.891.891 0 01.892-.891h1.485a.891.891 0 01.891.891v6.337c1.288-.933 2.593-2.056 3.628-3.406A1.496 1.496 0 0020.4 7.08 10.483 10.483 0 0010.632 0C4.811-.077 0 4.677 0 10.501a10.47 10.47 0 001.394 5.252 1.327 1.327 0 001.266.656c.28-.024.63-.06 1.046-.108a.742.742 0 00.659-.737V9.999M4.332 18.991a10.493 10.493 0 0016.641-9.21c-3.834 5.721-10.915 8.396-16.64 9.21"
+                    fill="#1A1A1A"
+                  />
+                </svg>
+              }
+              text="View on Etherscan"
+              url={`https://etherscan.io/token/${item.asset_contract.address}?a=${item.token_id}`}
+            />
             {item.external_link && (
               <ButtonIconExternal
                 icon={
