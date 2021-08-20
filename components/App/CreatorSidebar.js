48b2bd57c60d415728956d3dd7e6239a666b601e
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2021-08-19 19:40:38 -0500

    add bio and website to creator sidebar

--- components/App/CreatorSidebar.js
+++ components/App/CreatorSidebar.js
@@ -1,12 +1,19 @@
+import {
+  getCreatorName,
+  getCreatorBio,
+  getCreatorWebsite,
+} from '../../functions/utils'
 import CopyAddress from './CopyAddress'
 
 export default function CreatorHeader({ creator }) {
   return (
     <div className="px-6 py-2 w-80">
       <CopyAddress address={creator.address} />
-      <span className="text-2xl font-semibold">
-        {creator.name || 'Anonymous'}
+      <span className="py-4 block text-2xl font-semibold">
+        {getCreatorName(creator)}
       </span>
+      <span className="py-4 block text-md">{getCreatorBio(creator)}</span>
+      <span className="py-4 block text-md">{getCreatorWebsite(creator)}</span>
     </div>
   )
 }
