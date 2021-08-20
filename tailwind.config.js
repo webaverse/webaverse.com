1b9ef4e1b53cf449192951fb74f8506526178728
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2021-08-20 09:06:03 -0500

    extend styling for item header height

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        815: '815px',
+      },
+    },
   },
   variants: {
     extend: {
