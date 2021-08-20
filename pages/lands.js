eaddbfa9084f581454349772525490ca9db10d69
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2021-08-20 09:06:38 -0500

    pass in url for land item card

--- pages/lands.js
+++ pages/lands.js
@@ -21,7 +21,12 @@ export default function Lands({ items }) {
       />
       <div className="min-h-full min-w-full overflow-hidden grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
         {items.map((item) => (
-          <ItemCard key={`${item.hash}-${item.id}`} item={item} land />
+          <ItemCard
+            key={`${item.hash}-${item.id}`}
+            item={item}
+            land
+            href={`/lands/${item.id}`}
+          />
         ))}
       </div>
     </div>
