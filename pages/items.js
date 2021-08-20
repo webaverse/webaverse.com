6e096ff81816e674d819601162241e4279f51c0d
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2021-08-20 09:06:57 -0500

    pass in href for items item card

--- pages/items.js
+++ pages/items.js
@@ -21,7 +21,11 @@ export default function Items({ items }) {
       />
       <div className="min-h-full min-w-full overflow-hidden grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
         {items.map((item) => (
-          <ItemCard key={`${item.hash}-${item.id}`} item={item} />
+          <ItemCard
+            key={`${item.hash}-${item.id}`}
+            item={item}
+            href={`/items/${item.id}`}
+          />
         ))}
       </div>
     </div>
