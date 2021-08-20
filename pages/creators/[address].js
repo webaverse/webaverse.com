9cb66278ed869d7c8b30782c18843ca74c345667
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2021-08-19 19:40:00 -0500

    use creator functions and full profile on creator page

--- pages/creators/[address].js
+++ pages/creators/[address].js
@@ -1,26 +1,40 @@
-import { getCreator, getCreatorsItems } from '../../functions/api'
+import {
+  getAllCreatorsProfiles,
+  getCreatorsItems,
+  getOpenseaNfts,
+} from '../../functions/api'
 import ItemCard from '../../components/App/ItemCard'
 import CreatorHeader from '../../components/App/CreatorHeader'
 import CreatorSidebar from '../../components/App/CreatorSidebar'
-import { getNfts } from '../../functions/graphql'
 import SEO from '../../components/Shared/SEO'
-import { getRandomWebaverseAvatarImage } from '../../functions/utils'
+import { getCreatorName, getCreatorProfileImage } from '../../functions/creator'
 
 export default function CreatorPage({ creator, items }) {
+  const name = getCreatorName(creator)
+
   return (
     <div className="flex-grow">
       <SEO
-        title={`${creator.name || 'Anonymous'}'s account | Webaverse`}
-        description={`See ${creator.name || 'Anonymous'}'s Webaverse profile.`}
-        image={creator.avatarPreview || getRandomWebaverseAvatarImage()}
+        title={`${name}'s account | Webaverse`}
+        description={`See ${name}'s Webaverse profile.`}
+        image={getCreatorProfileImage(creator)}
       />
       <CreatorHeader creator={creator} />
       <div className="pt-12 lg:pt-80 flex lg:flex-nowrap flex-wrap">
         <CreatorSidebar creator={creator} />
-        <div className="px-4 min-h-full w-full overflow-hidden grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:gap-x-8">
-          {items.map((item) => (
-            <ItemCard key={`${item.hash}-${item.id}`} item={item} />
-          ))}
+        <div className="flex-1">
+          <div className="px-4 min-h-full w-full overflow-hidden grid grid-cols-1 gap-y-10 sm:grid-cols-2 gap-x-6 lg:grid-cols-3 xl:gap-x-8">
+            {items?.map((item) => (
+              <div key={`${item.id}`}>
+                <ItemCard item={item} />
+              </div>
+            ))}
+            {(!items || items.length < 1) && (
+              <h1 className="col-span-3 text-xl pt-4 mx-auto">
+                This user has no items.
+              </h1>
+            )}
+          </div>
         </div>
       </div>
     </div>
@@ -30,9 +44,9 @@ export default function CreatorPage({ creator, items }) {
 export async function getServerSideProps(context) {
   const { address } = context.query
 
-  const creator = await getCreator(address)
+  const creator = await getAllCreatorsProfiles(address)
   const webaverseItems = await getCreatorsItems(address)
-  const ethereumItems = (await getNfts(address, 1)) || []
+  const ethereumItems = (await getOpenseaNfts(address)) || []
 
   const items = [...webaverseItems, ...ethereumItems]
 
