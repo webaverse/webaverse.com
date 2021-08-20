82c05fb4c9b46263691b83c51f9591b8243c1593
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2021-08-20 14:43:26 -0500

    add ga and clarity

--- components/Shared/Analytics.js
+++ components/Shared/Analytics.js
@@ -6,15 +6,28 @@ export default function Analytics() {
       {/* Google Analytics */}
       <script
         async
-        src="https://www.googletagmanager.com/gtag/js?id=<YOUR_GA_ID_HERE>"
+        src="https://www.googletagmanager.com/gtag/js?id=G-DQT0N30X7D"
       />
       <script
         dangerouslySetInnerHTML={{
           __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '<YOUR_GA_ID_HERE>');
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          
+            gtag('config', 'G-DQT0N30X7D'); 
+          `,
+        }}
+      />
+      {/* Microsoft Clarity */}
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "838tbgka40");
           `,
         }}
       />
