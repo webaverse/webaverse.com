9eef67876b915297e15517ba17f1cea1a2ffcc8d
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2021-08-19 19:39:39 -0500

    add file ext util function and fix others

--- functions/utils.js
+++ functions/utils.js
@@ -1,10 +1,16 @@
-export function replaceIpfs(url) {
-  url
-    .replace('ipfs://Q', 'https://infura-ipfs.io/ipfs/Q')
-    .replace('ipfs://ipfs/', 'https://infura-ipfs.io/ipfs/')
-    .replace('https://ipfs.io/ipfs/', 'https://infura-ipfs.io/ipfs/')
+export function getFileExt(filename) {
+  return filename.split('.').pop()
+}
 
-  return url
+export function replaceIpfs(url) {
+  const replacedUrl = url
+    ? url
+        .replace('ipfs://Q', 'https://infura-ipfs.io/ipfs/Q')
+        .replace('ipfs://ipfs/', 'https://infura-ipfs.io/ipfs/')
+        .replace('https://ipfs.io/ipfs/', 'https://infura-ipfs.io/ipfs/')
+    : ''
+
+  return replacedUrl
 }
 
 export function replaceHttp(url) {
@@ -31,14 +37,14 @@ export function truncateEthAddress(address) {
 
 export function getRandomWebaverseImage() {
   const images = [
-    'https://docs.webaverse.com/assets/images/gift_silk-7d26f9c150d4db513e52c2fb6b3f389b.jpg',
-    'https://blog.webaverse.com/content/images/size/w2000/2021/08/blob.png',
-    'https://blog.webaverse.com/content/images/2021/08/image-10.png',
-    'https://blog.webaverse.com/content/images/size/w1000/2021/08/VRChat_dWPbbvrSnT.jpg',
+    // 'https://docs.webaverse.com/assets/images/gift_silk-7d26f9c150d4db513e52c2fb6b3f389b.jpg',
+    // 'https://blog.webaverse.com/content/images/size/w2000/2021/08/blob.png',
+    // 'https://blog.webaverse.com/content/images/2021/08/image-10.png',
+    // 'https://blog.webaverse.com/content/images/size/w1000/2021/08/VRChat_dWPbbvrSnT.jpg',
     'https://blog.webaverse.com/content/images/size/w2000/2021/07/SeasonSwitchNew_copy.jpg',
-    'https://blog.webaverse.com/content/images/size/w2000/2021/04/0bd04670b1e51acf3ba0e3eaad2d45f9-1.jpg',
-    'https://blog.webaverse.com/content/images/size/w1000/2021/01/thestreet.PNG',
-    'https://cdn.discordapp.com/banners/433492168825634816/080a87e705da9b3f9040486040177da9.jpg?size=2048',
+    // 'https://blog.webaverse.com/content/images/size/w2000/2021/04/0bd04670b1e51acf3ba0e3eaad2d45f9-1.jpg',
+    // 'https://blog.webaverse.com/content/images/size/w1000/2021/01/thestreet.PNG',
+    // 'https://cdn.discordapp.com/banners/433492168825634816/080a87e705da9b3f9040486040177da9.jpg?size=2048',
   ]
 
   return images[Math.floor(images.length * Math.random())]
