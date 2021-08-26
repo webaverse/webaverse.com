3f3af438e112cf62c3ebb101c89bf7523041d381
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2021-08-26 12:28:11 -0500

    fix webaverse api endpoint constant

--- constants/webaverse.ts
+++ constants/webaverse.ts
@@ -26,11 +26,11 @@ export const webaverseApiEndpoint = (() => {
       window.location.hostname === 'localhost' ||
       window.location.hostname === '127.0.0.1'
     ) {
-      return 'http://localhost:3000'
+      return `http://${window.location.hostname}:3000`
     }
-    return 'https://webaverse.com'
+    return `https://${window.location.hostname}`
   }
-  return 'http://localhost:3000'
+  return `http://localhost:3000`
 })()
 
 export const ghostBlogEndpoint =
