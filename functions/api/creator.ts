88297aa6420be5e6e52666a71dba70a651acae3e
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2021-08-25 20:27:12 -0500

    use constants in creator api functions

--- functions/api/creator.ts
+++ functions/api/creator.ts
@@ -1,14 +1,18 @@
+import {
+  webaverseAccountsEndpoint,
+  webaverseTokensEndpoint,
+} from '../../constants/webaverse'
 import { Creators, Creator } from '../../types/Creator'
 import { Items } from '../../types/Item'
 
 export async function getCreatorsItems(address: string): Promise<Items> {
-  return fetch(`https://tokens.webaverse.com/${address}`).then((res) =>
+  return fetch(`${webaverseTokensEndpoint}/${address}`).then((res) =>
     res.json(),
   )
 }
 
 export async function searchCreators(query: string): Promise<Creators> {
-  return fetch('https://accounts.webaverse.com/')
+  return fetch(webaverseAccountsEndpoint)
     .then((res) => res.json())
     .then((res) =>
       res.filter((creator: Creator) =>
@@ -18,13 +22,13 @@ export async function searchCreators(query: string): Promise<Creators> {
 }
 
 export async function getCreator(address: string): Promise<Creator> {
-  return fetch(`https://accounts.webaverse.com/${address.toLowerCase()}`).then(
+  return fetch(`${webaverseAccountsEndpoint}/${address.toLowerCase()}`).then(
     (res) => res.json(),
   )
 }
 
 export async function getCreators(): Promise<Creators> {
-  return fetch('https://accounts.webaverse.com/').then((res) => res.json())
+  return fetch(webaverseAccountsEndpoint).then((res) => res.json())
 }
 
 export async function getCreatorsWithBalance(): Promise<Creators> {
