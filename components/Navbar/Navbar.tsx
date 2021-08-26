e5cfc3ac812c102f53b00faa95866269ef071a78
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2021-08-26 13:48:22 -0500

    disable connect wallet text wrapping

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -91,7 +91,7 @@ const Navbar = (): JSX.Element => {
                   <div className="hidden lg:flex lg:items-center justify-center xl:col-span-2">
                     <a
                       href="#"
-                      className="items-center px-7 py-2 border border-transparent text-lg font-bold rounded-full shadow-sm text-white bg-black transform transition ease-in duration-100 hover:-translate-y-0.5 hover:shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2"
+                      className="items-center px-7 py-2 border border-transparent text-lg font-bold rounded-full shadow-sm text-white bg-black transform transition ease-in duration-100 hover:-translate-y-0.5 hover:shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2 whitespace-nowrap"
                       onClick={handleConnectClick}
                     >
                       Connect Wallet
