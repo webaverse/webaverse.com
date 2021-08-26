76e8e7b51731ca1a0dbb4a55b7e7056e7c9d209e
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2021-08-25 20:27:01 -0500

    use constants in item api functions

--- functions/api/item.ts
+++ functions/api/item.ts
@@ -1,17 +1,18 @@
+import { webaverseTokensEndpoint } from '../../constants/webaverse'
 import { Items, Item } from '../../types/Item'
 
 export async function searchItems(query: string): Promise<Items> {
-  return fetch(`https://tokens.webaverse.com/search?q=${query}`).then((res) =>
+  return fetch(`${webaverseTokensEndpoint}/search?q=${query}`).then((res) =>
     res.json(),
   )
 }
 
 export async function getItem(id: string): Promise<Item> {
-  return fetch(`https://tokens.webaverse.com/${id}`).then((res) => res.json())
+  return fetch(`${webaverseTokensEndpoint}/${id}`).then((res) => res.json())
 }
 
 export async function getItems(pageNum: number): Promise<Items> {
   return fetch(
-    `https://tokens.webaverse.com/${1 * pageNum}-${100 * pageNum}`,
+    `${webaverseTokensEndpoint}/${1 * pageNum}-${100 * pageNum}`,
   ).then((res) => res.json())
 }
