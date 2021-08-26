b338ef39a888a3eccf1ef47188b3a1594d269f30
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2021-08-25 20:27:32 -0500

    use constants in blog api functions

--- functions/api/blog.ts
+++ functions/api/blog.ts
@@ -1,16 +1,15 @@
+import { ghostBlogEndpoint } from '../../constants/webaverse'
 import { Blog, Blogs } from '../../types/Blog'
 
 export async function getBlogPosts(): Promise<Blogs> {
-  return fetch(
-    `https://webaverse.ghost.io/ghost/api/v3/content/posts/?key=${process.env.GHOST_API_KEY}`,
-  )
+  return fetch(`${ghostBlogEndpoint}/posts/?key=${process.env.GHOST_API_KEY}`)
     .then((res) => res.json())
     .then((res) => res.posts)
 }
 
 export async function getBlogPost(slug: string): Promise<Blog> {
   return fetch(
-    `https://webaverse.ghost.io/ghost/api/v3/content/posts/slug/${slug}/?key=${process.env.GHOST_API_KEY}`,
+    `${ghostBlogEndpoint}/posts/slug/${slug}/?key=${process.env.GHOST_API_KEY}`,
   )
     .then((res) => res.json())
     .then((res) => res.posts[0])
