7ca73530eb7174165f1f64c517b40efcbd897429
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2021-08-26 22:13:43 -0500

    add creator constant

---
+++ constants/creator.ts
@@ -0,0 +1,23 @@
+export const InitialCreatorState = {
+  name: '',
+  username: '',
+  address: '',
+  firstName: '',
+  avatarPreview: '',
+  profileImageUrl: '',
+  image: '',
+  homeSpacePreview: '',
+  coverImageUrl: '',
+  cover: '',
+  bio: '',
+  description: '',
+  website: '',
+  links: {
+    website: {
+      handle: '',
+    },
+  },
+  user: {
+    username: '',
+  },
+}
