1bfb3de80ff08dbf72105053e788f4cd89641169
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2021-08-27 12:56:11 -0500

    add fullscreen and 3d engine view for item header section

--- components/App/Item/ItemHeader.tsx
+++ components/App/Item/ItemHeader.tsx
@@ -1,4 +1,6 @@
 import Link from 'next/link'
+import { FullScreen, useFullScreenHandle } from 'react-full-screen'
+import { ArrowsExpandIcon } from '@heroicons/react/solid'
 import {
   getCreatorProfileImage,
   getCreatorShortName,
@@ -14,51 +16,100 @@ interface Props {
 }
 
 export default function ItemHeader({ item, creator }: Props): JSX.Element {
+  const handle = useFullScreenHandle()
+
   const itemImage = getItemImage(item)
   const itemImageExt = getFileExt(itemImage)
 
+  const is3d = (() => {
+    let is3dItem = false
+
+    if (!item?.ext) return is3dItem
+
+    switch (item.ext) {
+      case 'vrm':
+        is3dItem = true
+        break
+      case 'glb':
+        is3dItem = true
+        break
+      default:
+        is3dItem = false
+    }
+
+    return is3dItem
+  })()
+
   return (
     <>
-      <div className="shadow-inner bg-gray-300 relative lg:absolute top-0 left-0 w-full h-815 px-14 py-24">
-        <div className="mx-auto max-w-lg h-full max-h-full py-24 overflow-hidden">
-          {itemImageExt === 'mp4' ? (
-            <video
-              muted
-              autoPlay
-              loop
-              src={itemImage}
-              className="object-contain w-full h-full"
-            />
-          ) : (
-            <img
-              src={itemImage}
-              alt={`the item ${getItemName(item)}`}
-              // onError={addDefaultSrc}
-              className="object-contain w-full h-full"
-            />
-          )}
-        </div>
-      </div>
-      <div className="absolute lg:top-96">
-        <div className="absolute top-4">
-          <Link href={`/creators/${creator.address}`}>
-            <a>
-              <div className="flex align-middle items-center absolute -top-12 lg:top-96 h-14 w-44 rounded-full bg-white mx-4 shadow-sm transform transition ease-in duration-100 hover:-translate-y-0.5 hover:shadow-lg">
-                <div className="flex w-full px-2">
-                  <img
-                    className="h-10 w-10 rounded-full"
-                    src={getCreatorProfileImage(creator)}
-                    alt=""
+      <div className="shadow-inner lg:h-815 lg:absolute inset-0 w-full bg-gray-300 overflow-hidden" />
+      <div className="h-815 overflow-hidden pb-24">
+        <FullScreen className="h-full" handle={handle}>
+          <div className="relative w-full h-full bg-gray-300">
+            <div className="mx-auto w-full max-w-4xl h-full max-h-full py-24 overflow-hidden">
+              {is3d && (
+                <div className="h-full w-full">
+                  <iframe
+                    className="w-full h-full"
+                    title="item preview in Webaverse"
+                    src={`https://app.webaverse.com/?t=${item.id}&h=${item.hash}&e=${item.ext}`}
                   />
-                  <div className="pl-2 flex flex-col justify-center">
-                    <span className="font-semibold">
-                      {getCreatorShortName({ creator, length: 12 })}
-                    </span>
+                </div>
+              )}
+              {!is3d && itemImageExt === 'mp4' && (
+                <video
+                  muted
+                  autoPlay
+                  loop
+                  src={itemImage}
+                  className="object-contain w-full h-full"
+                />
+              )}
+              {!is3d && itemImageExt !== 'mp4' && (
+                <img
+                  src={itemImage}
+                  alt={`the item ${getItemName(item)}`}
+                  // onError={addDefaultSrc}
+                  className="object-contain w-full h-full"
+                />
+              )}
+            </div>
+          </div>
+        </FullScreen>
+        <div className="absolute lg:top-96">
+          <div className="absolute top-4">
+            <Link href={`/creators/${creator.address}`}>
+              <a>
+                <div className="flex align-middle items-center absolute -top-12 lg:top-96 h-14 w-44 rounded-full bg-white mx-4 shadow-sm transform transition ease-in duration-100 hover:-translate-y-0.5 hover:shadow-lg">
+                  <div className="flex w-full px-2">
+                    <img
+                      className="h-10 w-10 rounded-full"
+                      src={getCreatorProfileImage(creator)}
+                      alt=""
+                    />
+                    <div className="pl-2 flex flex-col justify-center">
+                      <span className="font-semibold">
+                        {getCreatorShortName({ creator, length: 12 })}
+                      </span>
+                    </div>
                   </div>
                 </div>
-              </div>
-            </a>
-          </Link>
+              </a>
+            </Link>
+          </div>
+        </div>
+        <div className="absolute lg:top-96 right-1/4">
+          <div className="absolute top-4">
+            <div className="flex align-middle items-center absolute -top-12 lg:top-96 h-14 w-14 rounded-full bg-white mx-4 shadow-sm transform transition ease-in duration-100 hover:-translate-y-0.5 hover:shadow-lg">
+              <button
+                className="w-full h-full p-3"
+                type="button"
+                onClick={handle.enter}
+              >
+                <ArrowsExpandIcon className="text-black" />
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </>
