7e41fe613dfa31d5be85b805e16307efb3994b0a
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2021-08-27 11:00:41 -0500

    get specific log event types and sort by timestamp

--- pages/api/ethereum/logs/[...asset].ts
+++ pages/api/ethereum/logs/[...asset].ts
@@ -1,6 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { openSeaEndpoint } from '../../../../constants/api'
 import { parseOpenSeaLogs } from '../../../../functions/helper/logs'
+import { ParsedLog, Logs } from '../../../../types/Log'
 
 export default async (
   req: NextApiRequest,
@@ -12,14 +13,29 @@ export default async (
 
   if (contractAddress) {
     try {
-      const creator = await fetch(
-        `${openSeaEndpoint}/events?asset_contract_address=${contractAddress}&token_id=${id}&only_opensea=false`,
-      )
-        .then((data) => data.json())
-        .then(async (data) => parseOpenSeaLogs(data.asset_events))
+      const urls = [
+        `${openSeaEndpoint}/events?asset_contract_address=${contractAddress}&token_id=${id}&only_opensea=false&limit=50&event_type=transfer`,
+        `${openSeaEndpoint}/events?asset_contract_address=${contractAddress}&token_id=${id}&only_opensea=false&limit=50&event_type=successful`,
+        // `${openSeaEndpoint}/events?asset_contract_address=${contractAddress}&token_id=${id}&only_opensea=false&limit=50&event_type=created`,
+        `${openSeaEndpoint}/events?asset_contract_address=${contractAddress}&token_id=${id}&only_opensea=false&limit=50&event_type=approve`,
+      ]
 
-      if (creator) {
-        res.status(200).json(creator)
+      const resultsArray: Logs = []
+      const promises = urls.map((url) =>
+        fetch(url)
+          .then((data) => data.json())
+          .then((data) => resultsArray.push(...data.asset_events)),
+      )
+      await Promise.all(promises)
+
+      const parsedResults = await parseOpenSeaLogs(resultsArray)
+      const sortedParsedResults = parsedResults.sort(
+        (a: ParsedLog, b: ParsedLog) =>
+          new Date(b.time).getTime() - new Date(a.time).getTime(),
+      )
+
+      if (sortedParsedResults) {
+        res.status(200).json(sortedParsedResults)
       } else {
         res.status(500).json({ error: 'logs not found' })
       }
