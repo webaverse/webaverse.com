3e73558c0a83bbad385b4f795f98a4230686a487
chrislatorres <29695350+chrislatorres@users.noreply.github.com>
2021-08-27 11:34:39 -0500

    do not use persist gate in SSR

--- pages/_app.js
+++ pages/_app.js
@@ -17,6 +17,26 @@ Router.events.on('routeChangeError', () => NProgress.done())
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 function MyApp({ Component, pageProps }) {
+  if (typeof window === 'undefined') {
+    return (
+      <Provider store={store}>
+        <TrackingProvider>
+          <div className="flex">
+            <Analytics />
+            <div className="flex-grow bg-gray-100">
+              <Navbar />
+              <div className="max-w-screen-2xl mx-auto min-h-screen pt-20">
+                {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+                <Component {...pageProps} />
+              </div>
+              <Footer />
+            </div>
+          </div>
+        </TrackingProvider>
+      </Provider>
+    )
+  }
+
   return (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
